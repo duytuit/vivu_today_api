a278c842c904b34fc4ea892abbfeefc6b2fdd8e6
Duy Tu <duytu89@gmail.com>
2023-05-09 22:23:08 +0700

    dev

--- app/Http/Controllers/Api/V2/ProductController.php
+++ app/Http/Controllers/Api/V2/ProductController.php
@@ -28,6 +28,7 @@ class ProductController extends Controller
     {
         $count = DB::table('products')->select('id')->count();
         $skip = $request->page;
+        $skip = ($skip * 2) - 2;
         $sdfsdf = DB::table('products')->skip($skip)->take(2)->get();
         return response()->json(['data'=>$sdfsdf,'count'=>$count]);
     }
